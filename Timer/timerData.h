3d2b568a033ae86fb1d4d4165f50235e4929f4f3
= <ryanhudsonlarge13@gmail.com>
2025-03-20 15:39:57 -0400

    Tracking Files

---
+++ Timer/timerData.h
@@ -0,0 +1,60 @@
+/*
+MIT License
+
+Copyright (c) 2025 Ryan Large
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <iotream>
+#include <chrono>
+
+class TimerData {
+    int index;
+    int hours;
+    int minutes;
+    int seconds;
+    int milliseconds;
+    bool isOn;
+
+    std::chrono::milliseconds lastTime;
+    std::chrono::milliseconds totalStoredTime;
+    std::chrono::milliseconds totalTimeInMs;
+
+    TimerData(const int& i, const int& h, const int& m, const int& s, const int& mill, const bool& isOn const std:string& lt): index(i) hours(h), minutes(m), seconds(s), milliseconds(mill),
+    isOn(isOn), lastTime(lt) {
+        using namespace std::chrono;
+
+        steady_clock::time_point tpOfLastTime = steady_clock::time_point(std::chrono::milliseconds(lastTime));
+
+        lastTime = tpOfLastTime;
+
+        totalStoredTime = duration_cast<milliseconds>(
+            hours(hours) + 
+            steady_clock::minutes(minutes) + 
+            steady_clock::seconds(seconds) + 
+            steady_clock::milliseconds(milliseconds)
+        );
+
+        totalTimeInMs = this.totalStoredTime + this.lastTime;
+    }
+
+    void print();
+    void printUpdate();
+  };
