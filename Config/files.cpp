d7e357d0d13eca90eceed09262885832e43ccf7c
Ryan Large <ryanhudslarge13@gmail.com>
2025-04-08 09:43:59 -0400

    Doxygen In Files

--- Config/files.cpp
+++ Config/files.cpp
@@ -32,18 +32,41 @@ SOFTWARE.
 #include "../includes/nlohmann/json.hpp"
 
 /**
-* @brief For better readability and development process 
+* @brief For better readability and development process using json is declared here at the top of the implementation file
 */
 using json = nlohmann::json;
 
 Files::Files() {}
 
+// Read alarms from file, create AlarmData instances from json returned from file and return AlarmData instances in a vector
 std::vector<AlarmData> Files::getAlarms() {
   std::string basePath = Files::getAppDataPath();
   std::filesystem::create_directories(basePath);
 
   std::ifstream alarmFile(basePath + "alarm.json");
 
+  /**
+  * @brief alarmFileData return type
+    {
+      "error": boolean // did the read fail or not  
+      "alarms": [ // array of AlarmData types @see AlarmData
+        {
+          "dow": int[],
+          "time": {
+            "hour": int,
+            "minute": int
+          },
+          "on": boolean,
+          "meridiem": "string",
+          "vibrate": {
+            "on": boolean,
+            "intensity": double
+          }
+        },
+        ...
+      ]
+    }
+  */
   json alarmFileData = deserializeJson(alarmFile);
 
   if (alarmFileData["error"] == true) {
@@ -58,12 +81,18 @@ std::vector<AlarmData> Files::getAlarms() {
 
   std::vector<AlarmData> alarms;
 
+  // For each field in alarms create the alarm in memory and add it to the alarms vector the function plans to return
+
+  /*
+    TODO:
+      1. Verify all this data and or return valid options if it does not exist before creating AlarmData object
+  */
   for (AlarmData alarm: alarmFileData["alarms"]) {
     
     std::vector<Alarm::DaysOfWeek> daysOfWeek = alarm["dow"];
 
-    std::string alarmTimeHours = alarm["time"]["hours"];
-    std::string alarmTimeMinutes = alarm["time"]["minutes"];
+    std::string alarmTimeHours = alarm["time"]["hour"];
+    std::string alarmTimeMinutes = alarm["time"]["minute"];
 
     bool on = alarm["on"];
     std::string meridiem = alarm["meridiem"];
@@ -73,6 +102,7 @@ std::vector<AlarmData> Files::getAlarms() {
 
     AlarmData::Vibrate vibrate(vibrateOn, intensity);
 
+    // Build the alarm with data pulled from file
     AlarmData existingAlarm = AlarmData(daysOfWeek, AlarmData::AlarmTime(alarmTimeHours, alarmTimeMinutes), on, meridiem, vibrate);
 
     // Loop through days of week to make sure that map contains the alarm and week day
@@ -83,6 +113,7 @@ std::vector<AlarmData> Files::getAlarms() {
 }
 
 // Timer methods ----------------------------------------------------------------------------------
+// Read timers as json from the timer config file stored on the users computer and create Timer objects to return to the caller of the method
 std::vector < TimerData > Files::getTimers() {
   std::string basePath = Files::getAppDataPath();
   std::filesystem::create_directories(basePath);
@@ -90,6 +121,23 @@ std::vector < TimerData > Files::getTimers() {
   std::ifstream timerFile(basePath + "timer.json");
 
   json timerFileData = deserializeJson(timerFile);
+
+  /**
+  * @brief timerFileData json data structure
+  {
+    "error": boolean,
+    "timers": [ // array of TimerData data. @see TimerData
+      {
+        "hours": int,
+        "minutes": int,
+        "seconds": int,
+        "running": boolean,
+        "paused": boolean
+      },
+      ...
+    ]
+  }
+  */
 
   std::vector < TimerData > times = {};
 
@@ -155,6 +203,8 @@ void Files::saveTimers(const std::vector<TimerData>& timers) {
 // Timer methods ----------------------------------------------------------------------------------
 
 
+// Store json of any shape into json nlohmann::json type and return it to the caller of the method
+// Catch errors of many sorts returning j with j["error"] set to true
 json Files::deserializeJson(std::ifstream& inFile) {
   json j;
 
@@ -182,6 +232,7 @@ json Files::deserializeJson(std::ifstream& inFile) {
   return j;
 };
 
+// Save any json type to specified file
 void Files::saveJsonToFile(const std::string& fileName, json j) {
     std::string basePath = Files::getAppDataPath();
     std::filesystem::create_directories(basePath);
